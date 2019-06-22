d45b04575889f82ecb042b095748ad42c5ee14ff
ox223252 <23252@laposte.net>
2019-06-22 16:36:36 +0200

    Add fd in the init function and improve README
    Modifications qui seront validées :
    	modifié :         README.md
    	modifié :         holonomic.c
    	modifié :         holonomic.h

--- holonomic.c
+++ holonomic.c
@@ -152,10 +152,18 @@ uint32_t holonomicSetDelay ( robot_t * const r, const uint32_t delay )
 	return ( tmp );
 }
 
-int holonomicInit ( robot_t *const r, const bool useThread, pthread_mutex_t * const busMutex )
+int holonomicInit ( robot_t *const r, const bool useThread, pthread_mutex_t * const busMutex, const int fd )
 {
 	if ( useThread )
 	{
+		r->steps = 0;
+
+		r->stepper.front.right = 0;
+		r->stepper.front.left = 0;
+		r->stepper.back.right = 0;
+		r->stepper.back.left = 0;
+		r->fd = fd;
+
 		r->busMutex = busMutex;
 		if ( busMutex )
 		{
